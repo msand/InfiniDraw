c3ffcf23681cc1d3ed91d7ad3a7bf86ddd5ba592
Mikael Sand <msand@abo.fi>
2018-05-12 03:52:31 +0300

    Implement graph.cool backend for persistence and real-time subscriptions

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
+import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
 import { Svg, G, Path, Rect } from 'react-native-svg';
 
+import initApollo from './lib/init-apollo';
 import DrawingComponentFactory from './lib/drawing';
 
 const App = DrawingComponentFactory(Svg, G, Path, Rect);
 
-export default App;
+const apolloClient = initApollo({}, true);
+
+class InfiniDraw extends Component {
+  render() {
+    return (
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
+    );
+  }
+}
+
+export default InfiniDraw;
