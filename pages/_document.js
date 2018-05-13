b6e206a7b332d3f126672270ca33cd9c3ae1c2d3
Mikael Sand <msand@abo.fi>
2018-05-13 23:36:32 +0300

    Prevent default pan, double click and zoom in ios safari

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,40 @@ import Document, { Head, Main, NextScript } from 'next/document';
 import React from 'react';
 import { AppRegistry } from 'react-native-web';
 
+if (process.browser) {
+  // prevent pan and zoom on safari
+  document.addEventListener('gesturestart',
+    e => {
+      e.preventDefault();
+    },);
+  document.documentElement.addEventListener(
+    'touchstart',
+    e => {
+      e.preventDefault();
+    },
+    false,
+  );
+  document.addEventListener(
+    'touchmove',
+    e => {
+      e.preventDefault();
+    },
+    false,
+  );
+  let lastTouchEnd = 0;
+  document.documentElement.addEventListener(
+    'touchend',
+    e => {
+      const now = new Date().getTime();
+      if (now - lastTouchEnd <= 300) {
+        e.preventDefault();
+      }
+      lastTouchEnd = now;
+    },
+    false,
+  );
+}
+
 let index = 0;
 
 // Force Next-generated DOM elements to fill their parent's height.
@@ -9,10 +43,12 @@ let index = 0;
 // layout for top-level wrapping elements.
 const normalizeNextElements = `
   html, body {
-    height: 100%;
     width: 100%;
+    height: 100%;
+    position: fixed;
     overflow: hidden;
     overscroll-behavior: none;
+    -webkit-overflow-scrolling: touch;
   }
   body > div:first-child,
   #__next {
@@ -40,7 +76,10 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <title>InfiniDraw</title>
-          <meta name="viewport" content="user-scalable=no, width=device-width" />
+          <meta
+            name="viewport"
+            content="user-scalable=no, width=device-width"
+          />
         </Head>
         <body>
           <Main />
