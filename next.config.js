2f2ce96df29b6404808e0dd5814c6cffc0353e1c
Mikael Sand <msand@abo.fi>
2019-02-10 20:43:36 +0200

    Upgrade next

--- next.config.js
+++ next.config.js
@@ -1,40 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies,no-param-reassign */
-const babelCore = require('@babel/core');
-
-const { createConfigItem } = babelCore;
-
-const presetItem = createConfigItem(
-  require('next/dist/server/build/babel/preset'),
-  { type: 'preset' },
-);
-const hotLoaderItem = createConfigItem(require('react-hot-loader/babel'), {
-  type: 'plugin',
-});
-const reactJsxSourceItem = createConfigItem(
-  require('@babel/plugin-transform-react-jsx-source'),
-  { type: 'plugin' },
-);
-
-function babelConfig({ isServer, dev }) {
-  return {
-    cacheDirectory: true,
-    presets: [presetItem],
-    plugins: [
-      dev && !isServer && hotLoaderItem,
-      dev && reactJsxSourceItem,
-      [
-        'babel-plugin-module-resolver',
-        {
-          alias: {
-            '^react-native$': 'react-native-web',
-          },
-        },
-      ],
-    ].filter(Boolean),
-    babelrc: false,
-  };
-}
-
 // Update these to match your package scope name.
 const internalNodeModulesRegExp = /(?:zoomable-svg|react-native-svg|react-native-color|react-native-slider)(?!.*node_modules)/;
 const externalNodeModulesRegExp = /node_modules(?!\/(?:zoomable-svg|react-native-color|react-native-svg|react-native-slider)(?!.*node_modules))/;
@@ -47,7 +10,14 @@ module.exports = {
       return (ctx, req, cb) =>
         internalNodeModulesRegExp.test(req) ? cb() : external(ctx, req, cb);
     });
-    defaultLoaders.babel.options = babelConfig({ isServer, dev });
+    defaultLoaders.babel.options.plugins = [[
+      'babel-plugin-module-resolver',
+      {
+        alias: {
+          '^react-native$': 'react-native-web',
+        },
+      },
+    ]];
     config.module.rules.push({
       test: /\.+(js|jsx)$/,
       loader: defaultLoaders.babel,
